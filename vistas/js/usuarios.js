9a850f3b298c37e61c16a2ecfae326441c2887e5
Rodrigo Chavez <oscar.rodrigo.chavez@gmail.com>
2020-10-08 11:36:20 -0500

    commit al proyecto completo

---
+++ vistas/js/usuarios.js
@@ -0,0 +1,92 @@
+/*=============================================
+SUBIENDO LA FOTO DEL USUARIO
+=============================================*/
+
+$(".nuevaFoto").change(function(){
+
+
+	var imagen = this.files[0];
+
+	/*=============================================
+	VALIDAMOS EL FORMATO DE LA IMAGEN SEA JPG O PNG
+	=============================================*/
+
+	if(imagen["type"] != "image/jpeg" && imagen["type"] != "image/png"){
+
+		$(".nuevaFoto").val("");
+
+		swal({
+			 title: "Error al subir la imagen",
+			 text: "!La imagen debe estar en formato JPG o PNG!",
+			 type: "error",
+			 confirmButtonText: "!Cerrar!"
+		});
+
+	}else if(imagen["size"] > 2000000){
+
+		$(".nuevaFoto").val("");
+
+		swal({
+			 title: "Error al subir la imagen",
+			 text: "!La imagen no debe pesar mas de 2MB!",
+			 type: "error",
+			 confirmButtonText: "!Cerrar!"
+		});
+
+	}else{
+
+		var datosImagen = new FileReader;
+		datosImagen.readAsDataURL(imagen);
+
+		$(datosImagen).on("load", function(event){
+
+			var rutaImagen = event.target.result;
+
+			$(".previsualizar").attr("src", rutaImagen);
+		})
+	}
+	
+})
+
+	/*=============================================
+	EDITAR USUARIO
+	=============================================*/
+
+	$(".btnEditarUsuario").click(function(){
+
+		var idUsuario = $(this).attr("idUsuario");
+
+		var datos = new FormData();
+		datos.append("idUsuario", idUsuario);
+
+		$.ajax({
+
+			url:"ajax/usuarios.ajax.php",
+			method: "POST",
+			data: datos,
+			cache: false,
+			contentType: false,
+			processData: false,
+			dataType: "json",
+			success: function(respuesta){
+
+				$("#editarNombre").val(respuesta["nombre"]);
+				$("#editarUsuario").val(respuesta["usuario"]);
+				$("#editarPerfil").html(respuesta["perfil"]);
+				$("#editarPerfil").val(respuesta["perfil"]);
+				$("#fotoActual").val(respuesta["foto"]);
+
+				$("#passwordActual").val(respuesta["password"]);
+
+				if(respuesta["foto"] != ""){
+
+					$(".previsualizar").attr("src", respuesta["foto"]);
+				}
+
+			}
+
+
+		});
+	})
+
+
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2020 <a href="https://
+		www.adatem.com" target="_blank">Analisis de Datos Empresariales</a>.
+	</strong>
+
+	Todos los derechos reservados.
+
+</footer>
